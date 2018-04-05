463229a01c34fee610abccb181bf18bb399d80af
jorgemspereira <jmspereira125@gmail.com>
2018-04-05 12:27:30 +0100

    Changed main name. Updated gradle settings. Removed useless test.

---
+++ src/main/java/ist/meic/pa/GenericFunctions/WithGenericFunctions.java
@@ -0,0 +1,11 @@
+package ist.meic.pa.GenericFunctions;
+
+public class WithGenericFunctions {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.print("Usage java -cp genericFunctions.jar WithGenericFunctions ClassName");
+        } else {
+            System.out.print("TODO");
+        }
+    }
+}
