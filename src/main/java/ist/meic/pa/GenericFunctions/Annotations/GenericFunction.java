d5da485e4496ff0ad88665e30386f6e5d31b11e8
jorgemspereira <jmspereira125@gmail.com>
2018-04-05 11:52:53 +0100

    Annotations added

---
+++ src/main/java/ist/meic/pa/GenericFunctions/Annotations/GenericFunction.java
@@ -0,0 +1,8 @@
+package ist.meic.pa.GenericFunctions.Annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface GenericFunction {
+}
